17ace8d296ab867a25151b55be9c263652caef88
PC <git@github.com:denis-poroshin/store1.git>
2024-10-02 11:30:41 +0300

    creating a project Star

---
+++ src/main/java/ru/star/springbankstar/SpringBankStarApplication.java
@@ -0,0 +1,13 @@
+package ru.star.springbankstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBankStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBankStarApplication.class, args);
+	}
+
+}
